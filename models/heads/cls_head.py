0d48ee98e91f85674dfb96129786aaba79dfcc29
wj <nawonjae@naver.com>
2025-06-27 19:11:18 +0900

    update cls_head

--- models/heads/cls_head.py
+++ models/heads/cls_head.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from typing import Sequence, Union
+from typing import Sequence
 
 from models.utils.dat_blocks import LayerNormProxy
 
@@ -11,24 +11,29 @@ class ClsHead(nn.Module):
 
     def __init__(
         self,
-        in_channels: int,
+        in_channels: Sequence[int],
         num_classes: int,
         *,
         dropout_ratio: float = 0.0,
     ) -> None:
         super().__init__()
-        self.norm = LayerNormProxy(in_channels)
+
+        self.norms = nn.ModuleList([LayerNormProxy(c) for c in in_channels])
+        total_channels = sum(in_channels)
+
         self.pool = nn.AdaptiveAvgPool2d(1)
         self.dropout = nn.Dropout(dropout_ratio) if dropout_ratio > 0 else nn.Identity()
-        self.fc = nn.Linear(in_channels, num_classes)
+        self.fc = nn.Linear(total_channels, num_classes)
 
-    def forward(self, x: Union[torch.Tensor, Sequence[torch.Tensor]]):  # type: ignore
-        if isinstance(x, (list, tuple)):
-            x = x[-1]
+    def forward(self, x: Sequence[torch.Tensor]):
+        outs = []
+        for i, feat in enumerate(x):
+            y = self.norms[i](feat)
+            y = self.pool(y)
+            y = torch.flatten(y, 1)
+            outs.append(y)
+        x = torch.cat(outs, dim=1)
 
-        x = self.norm(x)
-        x = self.pool(x)
-        x = torch.flatten(x, 1)
         x = self.dropout(x)
         logits = self.fc(x)
         return logits 
